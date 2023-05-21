252ca2e2297014855f957618a2165e0ccdcc5aec
kiji-haru <kijiharu3112@outlook.jp>
2023-05-22 05:57:38 +0900

    feat:他のページ

--- main.rb
+++ main.rb
@@ -1,81 +1,122 @@
+# this program is write by ruby
+# this program is "kuso" code
+# if you feel "kuso", don't overdo and take a rest
+$serif = [
+  "thanks for play this game",
+  "well, i have a good news for you",
+  "do you know 'LoveLive!' ?",
+  "It's very very good anima and game",
+  "recently, release the new LoveLive's game from bandai",
+  "so i hope you to enjoy playing this game",
+  "please please please please please ",
+  "do do do do do do do do do do ",
+  "you must play this game!!!!!!!!!!!!!!!!!!!!!!!!!!",
+  "unofficial LoveLive",
+]
 require 'dxopal'
 include DXOpal
+
 Window.load_resources do
-
+  $seen = 3
+  $time = 0
   Window.bgcolor = C_BLACK
-  img_bar = Image.new(100, 20, C_WHITE)
-  img_hwall = Image.new( 20, 480, C_BLUE)
-  img_vwall = Image.new(630, 20, C_BLUE)
-  img_ball = Image.new(20, 20).circle_fill(10, 10, 10, C_RED)
-  img_block = Image.new(58, 18, C_GREEN)
-  img_block_y = Image.new( 58, 18, C_YELLOW)
-
-
-  bar = Sprite.new(0, 460, img_bar)
-  lwall = Sprite.new(0, 0, img_hwall)
-  rwall = Sprite.new(620, 0, img_hwall)
-  twall = Sprite.new(0, 0, img_vwall)
-  walls = [bar, lwall, rwall, twall]
-  ball = Sprite.new(300, 400, img_ball)
-
-  dx = 5
-  dy = -5
-  def move(sprite, speed_x, speed_y)
-    sprite.x += speed_x
-    sprite.y += speed_y
-  end
-
-  blocks = []
-  10.times do |x|
-    5.times do |y|
-      blocks << Sprite.new(21 + 60 * x, 21 + 20 * y, img_block)
-    end
-  end
-
   font = Font.new(20)
   Window.loop do
-    if Input.key_down?(K_A)
-      bar.x -= 200
-    elsif Input.key_down?(K_D)
-      bar.x += 200
+    if Input.key_push?(K_SPACE)
+      $seen = 1
     end
+    img_bar = Image.new(100, 20, C_WHITE)
+    img_hwall = Image.new( 20, 480, C_BLUE)
+    img_vwall = Image.new(630, 20, C_BLUE)
+    img_ball = Image.new(20, 20).circle_fill(10, 10, 10, C_RED)
+    img_block = Image.new(58, 18, C_GREEN)
+    img_block_y = Image.new( 58, 18, C_YELLOW)
+    if $seen == 0
+      Window.draw_font(200, 200, "push space key to start", font)
+    elsif $seen == 1
+      $dx = 5
+      $dy = -5
+      def move(sprite, speed_x, speed_y)
+        sprite.x += speed_x
+        sprite.y += speed_y
+      end
+      $bar = Sprite.new(0, 460, img_bar)
+      lwall = Sprite.new(0, 0, img_hwall)
+      rwall = Sprite.new(620, 0, img_hwall)
+      twall = Sprite.new(0, 0, img_vwall)
+      $walls = [$bar, lwall, rwall, twall]
+      $ball = Sprite.new(300, 400, img_ball)
 
-    Sprite.draw(walls)
+      $blocks = []
+      10.times do |x|
+        5.times do |y|
+          $blocks << Sprite.new(21 + 60 * x, 21 + 20 * y, img_block)
+        end
+      end
 
-    move(ball, dx, 0)
-    if ball === walls
-      ball.x -= dx
-      dx = -dx
-    end
-    coll_x = ball.check(blocks)
-    if coll_x[0]
-      coll_x[0].image = img_block_y
-      coll_x[0].draw
-      coll_x[0].vanish
-      ball.x -= dx
-      dx = -dx
-    end
+      $seen = 2
+    elsif $seen == 2
+      if Input.key_down?(K_A)
+        $bar.x -= 200
+      elsif Input.key_down?(K_D)
+        $bar.x += 200
+      end
 
-    move(ball, 0, dy)
-    if ball === walls
-      ball. y -= dy
-      dy = -dy
-    end
-    coll_y = ball.check(blocks)
-    if coll_y[0]
-      coll_y[0].image = img_block_y
-      coll_y[0].draw
-      coll_y[0].vanish
-      ball. y -= dy
-      dy = -dy
-    end
+      Sprite.draw($walls)
+      move($ball, $dx, 0)
+      if $ball === $walls
+        $ball.x -= $dx
+        $dx = -$dx
+      end
+      coll_x = $ball.check($blocks)
+      if coll_x[0]
+        coll_x[0].image = img_block_y
+        coll_x[0].draw
+        coll_x[0].vanish
+        $ball.x -= $dx
+        $dx = -$dx
+      end
 
-    ball.draw
+      move($ball, 0, $dy)
+      if $ball === $walls
+        $ball.y -= $dy
+        $dy = -$dy
+      end
+      coll_y = $ball.check($blocks)
+      if coll_y[0]
+        coll_y[0].image = img_block_y
+        coll_y[0].draw
+        coll_y[0].vanish
+        $ball.y -= $dy
+        $dy = -$dy
+      end
 
-    Sprite.draw(blocks)
+      $ball.draw
 
-    if ball.y >= 480
-      break
+      Sprite.draw($blocks)
+      if $blocks.size == 0
+        $seen = 3
+        $time = 0
+        break
+      end
+      if $ball.y >= 480
+        $seen = 4
+        break
+      end
+      Sprite.clean($blocks)
+      Window.draw_font(200,200, `#{$blocks.size}`, font)
+    elsif $seen == 3
+      Window.draw_font(200, 100, `#{$time}`, font)
+      $time += 1
+      9.times do |i|
+        Window.draw_font(50, 400 + 100 * i - $time, $serif[i], font)
+      end
+      if $time >= 1000
+        $seen = 0
+      end
+
+    elsif $seen == 4
+      Window.draw_font(200, 200, "well... good luck", font)
     end
   end
 end
